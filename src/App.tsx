cb155834c95cbe87ff9c4805f4df413f838924b4
ronaldoss8 <ronaldosssantos1@gmail.com>
2025-02-23 23:16:51 -0300

    addin new pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import Home from "./pages/Home/Home"
 
 
 const App=() => {
@@ -6,6 +6,7 @@ const App=() => {
   return (
     <>
      myport
+     <Home/>
     </>
   )
 }
---
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -0,0 +1,17 @@
+import { styled } from "@mui/material"
+
+const Hero=() => {
+  const StyledHero = styled("div")(()=>({
+
+}))
+
+    return (
+     
+      
+      <>
+     Hero
+      </>
+    )
+  }
+  
+  export default Hero
